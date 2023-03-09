f22983635af4d9d1aa2cdd8c39066dc7cb908aa7
theHollowCode <microlchen0616@gmail.com>
2023-03-08 22:11:31 -0500

    nodesignjustfuncforNow

---
+++ src/components/subpages/featuredrawer1.js
@@ -0,0 +1,96 @@
+import styles from '@/styles/Home.module.css'
+import * as React from 'react';
+import { List, Box, Link, ListItem, Button } from "@mui/material/";
+import WriteToCloudFirestore from '@/components/cloudFirestore/Write.js';
+import ReadToCloudFirestore from '@/components/cloudFirestore/Read.js';
+
+
+
+const Featuredrawer1 = () => {
+    //make a hashmap
+    
+
+    return (
+        <Box sx={{ marginTop: 7, marginLeft: 7 }}>
+            <div className={styles.questionheading}>Over the last 2 weeks, how often have you been bothered
+                by any of the following problems?</div>
+
+
+            <List >
+                <ListItem className={styles.questions}> Q1 - Little interest or pleasure in doing things</ListItem>
+                <Link className={styles.button}>0 - Not at all</Link>
+                <Link className={styles.button}>1 - Several days</Link>
+                <Link className={styles.button}>2 - More than half the days</Link>
+                <Link className={styles.button}>3 - Nearly every day</Link>
+            </List>
+            <List>
+                <ListItem className={styles.questions}> Q2 - Feeling down, depressed, or hopeless</ListItem>
+                <Link className={styles.button}>0 -  Not at all</Link>
+                <Link className={styles.button}>1 - Several days</Link>
+                <Link className={styles.button}>2 - More than half the days</Link>
+                <Link className={styles.button}>3 - Nearly every day</Link>
+            </List>
+            <List>
+                <ListItem className={styles.questions}> Q3 - Trouble falling or staying asleep, or sleeping too much</ListItem>
+                <Link className={styles.button}>0 -  Not at all</Link>
+                <Link className={styles.button}>1 - Several days</Link>
+                <Link className={styles.button}>2 - More than half the days</Link>
+                <Link className={styles.button}>3 - Nearly every day</Link>
+            </List>
+            <List>
+                <ListItem className={styles.questions}> Q4 - Feeling tired or having little energy</ListItem>
+                <Link className={styles.button}>0 -  Not at all</Link>
+                <Link className={styles.button}>1 - Several days</Link>
+                <Link className={styles.button}>2 - More than half the days</Link>
+                <Link className={styles.button}>3 - Nearly every day</Link>
+            </List>
+            <List>
+                <ListItem className={styles.questions}> Q5 - Poor appetite or overeating </ListItem>
+                <Link className={styles.button}>0 -  Not at all</Link>
+                <Link className={styles.button}>1 - Several days</Link>
+                <Link className={styles.button}>2 - More than half the days</Link>
+                <Link className={styles.button}>3 - Nearly every day</Link>
+            </List>
+            <List>
+                <ListItem className={styles.questions}> Q6 - Feeling bad about yourself — or that you are a failure or 
+have let yourself or your family down </ListItem>
+                <Link className={styles.button}>0 -  Not at all</Link>
+                <Link className={styles.button}>1 - Several days</Link>
+                <Link className={styles.button}>2 - More than half the days</Link>
+                <Link className={styles.button}>3 - Nearly every day</Link>
+            </List>
+            <List>
+                <ListItem className={styles.questions}> Q7 - Trouble concentrating on things, such as reading the 
+newspaper or watching television</ListItem>
+                <Link className={styles.button}>0 -  Not at all</Link>
+                <Link className={styles.button}>1 - Several days</Link>
+                <Link className={styles.button}>2 - More than half the days</Link>
+                <Link className={styles.button}>3 - Nearly every day</Link>
+            </List>
+            <List>
+                <ListItem className={styles.questions}> Q8 - Moving or speaking so slowly that other people could have 
+noticed? Or the opposite — being so fidgety or restless 
+that you have been moving around a lot more than usual </ListItem>
+                <Link className={styles.button}>0 -  Not at all</Link>
+                <Link className={styles.button}>1 - Several days</Link>
+                <Link className={styles.button}>2 - More than half the days</Link>
+                <Link className={styles.button}>3 - Nearly every day</Link>
+            </List>
+            <List>
+                <ListItem className={styles.questions}> Q9 - Thoughts that you would be better off dead or of hurting 
+yourself in some way </ListItem>
+                <Link className={styles.button}>0 -  Not at all</Link>
+                <Link className={styles.button}>1 - Several days</Link>
+                <Link className={styles.button}>2 - More than half the days</Link>
+                <Link className={styles.button}>3 - Nearly every day</Link>
+            </List>
+            <Box sx={{ display: 'flex', justifyContent: 'center', marginLeft: -7 }}>
+                <Button  className={styles.submit}>Submit</Button>
+                <WriteToCloudFirestore />
+                <ReadToCloudFirestore />
+            </Box>
+
+        </Box>
+    )
+}
+export default Featuredrawer1
---
+++ src/components/subpages/toolbar.js
@@ -0,0 +1,115 @@
+import styles from '@/styles/Home.module.css'
+import * as React from 'react';
+import { useState } from 'react';
+import { Link, List, ListItem, ListItemButton, ListItemText, ListItemIcon, Drawer, Toolbar, Box } from "@mui/material/";
+import Featuredrawer1 from './featuredrawer1';
+import MenuIcon from '@mui/icons-material/Menu';
+import { detectOverflow } from '@popperjs/core';
+
+const Toolbars = () => {
+
+    const [anchorDrawer, setAnchorDrawer] = useState(null);
+    const openDrawer = (event) => {
+        setAnchorDrawer(event.currentTarget);
+    }
+    const closeDrawer = () => {
+        setAnchorDrawer(null);
+    }
+
+    const [firstDrawer, setfirstDrawer] = useState(null);
+    const openfirstDrawer = (event) => {
+        setfirstDrawer(event.currentTarget);
+    }
+    const closefirstDrawer = () => {
+        setfirstDrawer(null);
+    }
+
+    return (
+
+        <Toolbar className={styles.appbar} sx={{ border: 1, borderColor: 'white' }}>
+            <Link onClick={openDrawer} sx={{ color: 'white' }}><MenuIcon /></Link>
+            <Drawer
+                anchor='left'
+                open={Boolean(anchorDrawer)}
+                onClose={closeDrawer}
+
+                PaperProps={{
+                    sx: {
+                        marginTop: "100px",
+                        backgroundColor: "grey",
+                        color: "white",
+                        flexGrow: 1,
+                        width: "100%"
+                    }
+                }}
+            >
+
+                <Box sx={{ mt: 3 }}>
+                    <List sx={{ ml: 2 }}>
+                        <ListItem disablePadding sx={{ marginLeft: 2 }}>
+                            <MenuIcon />
+                            <div className={styles.horizontal_spacing}></div>
+                            <Link underline="hover" onClick={openfirstDrawer} sx={{ color: 'white' }} >
+                                <ListItemText primaryTypographyProps={{ sx: { fontSize: '24px', textTransform: 'none' } }}>
+                                    sJKklas </ListItemText>
+                            </Link>
+                            <Drawer
+                                anchor='left'
+                                open={Boolean(firstDrawer)}
+                                onClose={closefirstDrawer}
+
+                                PaperProps={{
+                                    sx: {
+                                        //marginTop: "100px",
+                                        backgroundColor: "gray",
+                                        color: "black",
+                                        flexGrow: 1,
+                                        width: "75%"
+                                        
+                                    }
+                                }}
+                            >
+                                <Featuredrawer1 />
+                            </Drawer>
+                        </ListItem>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon><MenuIcon /></ListItemIcon>
+                                <ListItemText primary={"f2"} />
+                            </ListItemButton>
+                        </ListItem>
+
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon><MenuIcon /></ListItemIcon>
+                                <ListItemText primary={"f3"} />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+
+                    <List sx={{ ml: 2 }}>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemText primary={"f4"} />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem disablePadding>
+                            <ListItemButton >
+
+                                <ListItemText primary={"f5"} />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemText primary={"f6"} />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+
+                </Box>
+            </Drawer>
+        </Toolbar>
+    )
+}
+
+export default Toolbars
